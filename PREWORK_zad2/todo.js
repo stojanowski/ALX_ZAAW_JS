f40cfa6f4a161af6bb6506b2caf4699e088d74ad
Adam <adam.stojanowski@adatec.pl>
2022-03-19 11:40:13 +0100

    again

--- PREWORK_zad2/todo.js
+++ PREWORK_zad2/todo.js
@@ -1,5 +1,5 @@
 function btClick() {
     let listaUL = document.getElementById("listaToDo");
-    let task = document.getElementById("taskTxt");
-    listaUL.innerHTML += "<li>a "+task.innerText+"</li>";
+    let task = document.getElementById("taskTxt").value;
+    listaUL.innerHTML += "<li>a "+task+"</li>";
 };
